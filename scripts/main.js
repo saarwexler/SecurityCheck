707a0da2d0aa6e066e691c3e48e916db5be530e0
Snezhana Goldshteyn <sgoldsht@akamai.com>
2019-11-23 10:22:51 +0200

    Added changes from Eli's project

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,18 @@
 $("#startCheck").click(function() {
     $("#main").css("display", "none");
     $("body").css('background-color', "whitesmoke");
-    $("#result").css("display", "block")
+    $("#result").css("display", "block");
     let count = 1;
     while (count <= 10) {
         count = runTest(count);
     }
+    testResult("test1", 1);
 });
 
 
 function runTest(count) {
-        test(count);
-        return count+1;
+    test(count);
+    return count+1;
 }
 
 function test(count) {
@@ -22,3 +23,15 @@ function test(count) {
         $("#loader-info").text(parseInt(percent) + "%");
     }, 2000);
 }
+
+function testResult (id, answer) {
+    let element = document.getElementById(id);
+    console.log("test");
+    if (answer === 0) {
+        element.src = "../images/fail1.png";
+    } else {
+        element.src = "../images/pass1.png";
+    }
+}
+
+
