99ceb3e46f398d03bac73cf7f79f59cba94de0ad
nisht1 <46297871+nisht1@users.noreply.github.com>
2020-02-03 13:03:01 +0200

    Add files via upload
    Fix UI

--- scripts/main.js
+++ scripts/main.js
@@ -9,12 +9,60 @@
             $("#main").css("display", "none");
             $("body").css('background-color', "whitesmoke");
             $("#result").css("display", "block");
-            let count = 1;
-            for (let id of ids) {
-               run_test(id).then( result =>  testResult(id, result)).then(test(count))
-               count++;
-            }
+
+
+            runAll();
+            // let count = 1;
+            // for (let id of ids) {
+            //    run_test(id)
+            //     .then(result => {
+            //         console.log(result);
+            //         testResult(id, result)
+            //     })
+            //     .then(test(count))
+            //    count++;
+            // }
         });
+
+        
+
+        async function runAll() {
+            let urlsList = {
+                test1: 'http://akamaietpphishingtest.com',
+                test2: 'http://akamaietpphishingtest.com',
+                test3: 'http://akamaietpphishingtest.com',
+                test4: 'http://akamaietpphishingtest.com',
+                test5: 'http://akamaietpcompromisedmalwaretest.com/knownbadmacro.xlsm',
+                test6: 'http://akamaietpcompromisedmalwaretest.com/knownbadmalware.exe',
+                test7: 'http://akamaietpcompromisedphishingtest.com/knownbadphisihing.html',
+                test8: 'http://akamaietpcompromisedcnctest.com/knownbadcnc.html',          
+                test9: 'http://akamaietpcompromisedmalwaretest.com/badscript.js',
+                test10: 'http://akamaietpcompromisedmalwaretest.com/badscript.js',
+                test11: 'http://akamaietpcompromisedmalwaretest.com/badscript.js'
+            };
+            const urls = Object.values(urlsList);
+
+            let index = 1;
+            for (const url of urls) {
+                await delay(1500).then(() => {
+                    fetch(url, {}, 5000)
+                    .then(response => response.url.indexOf("error") > -1)
+                    .then(result => {
+                        testResult(`test${index}`, result);
+                    })
+                    .then(() =>  {
+                        console.log("index++", index);
+                        index++;
+                        test(index);
+                    });
+                })
+                
+            }
+        }
+
+        function delay(ms) {
+            return new Promise(resolve => setTimeout(resolve, ms));
+        }
 
         let run_test = function (id, count) {
           let map = new Map([["test1",'http://akamaietpphishingtest.com'],
@@ -37,14 +85,18 @@
 
         function test(count) {
             let total = 11;
-            let percent = count / total * 100 + "%";
+            console.log(count, total, (count / total) * 100);
+            let result = Math.floor((count / total) * 100);
+            result = result < 100 ? result : 100;
+            let percent = result + "%";
+
             $("#loader-bar").css("width", percent);
             $("#loader-info").text(parseInt(percent) + "%");
         }
 
         function testResult(id, answer) {
-            let element = document.getElementById(id);
-            console.log( id + ": " +answer)
+                let element = document.getElementById(id);
+            console.log( id + ": " +answer);
             if (answer === false) {
                 element.src = "http://www.proredetelecom.com.br/wp-content/uploads/2019/01/icon-fail.png";
                 if (id.slice(4) <= 8) {
@@ -67,4 +119,5 @@
                 console.log("passed");
             }
             return Promise.resolve(id);
+            
         }
